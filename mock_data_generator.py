1e59565296005ee6407188222157dd3ba0b5175d
Nenagoossens <goossens@hotitem.nl>
2025-04-08 19:32:05 +0200

    Update mock_data_generator.py

--- mock_data_generator.py
+++ mock_data_generator.py
@@ -2,7 +2,7 @@ import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies
 import plotly.express as px
 '''
 # Lees de CSV
